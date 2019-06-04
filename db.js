0e9c65f7ca53d303397893cf70f71edbadd8ebef
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-06-04 10:27:54 +0200

    setup

--- db.js
+++ db.js
@@ -1,24 +1,17 @@
-const { Pool } = require('pg')
-const Sequelize = require('sequelize')
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:secret@localhost:5432/postgres'
-const pool = new Pool({ connectionString: connectionString })
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(connectionString, {define: { timestamps: false }})
+const connectionString =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
+const sequelize = new Sequelize(connectionString, {
+  define: { timestamps: false }
+});
 
-pool.on('error', (err, client) => {
-  console.error('error event on pool', err)
-})
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Sequelize updated database schema");
+  })
+  .catch(console.error);
 
-// EXAMPLE TABLE QUERY
-//pool.query(`
-  // CREATE TABLE IF NOT EXISTS person
-  //   (
-  //     id SERIAL,
-  //     first_name VARCHAR(255),
-  //     last_name VARCHAR(255),
-  //     eye_color VARCHAR(255)
-  //   );
-//`)
-//  .catch(console.error)
-
-module.exports = sequelize
+module.exports = sequelize;
